2f8863d90d3c8f7683db0377a41050131f1f01f6
huinki <80573916+huinki@users.noreply.github.com>
2021-03-13 18:53:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,23 +1,24 @@
-from discord.ext import commands
-import os
-import traceback
+#coding:UTF-8
+import discord
+from discord.ext import tasks
+from datetime import datetime 
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+TOKEN = "ODE5NzM5MjkzOTY1ODc3MjU4.YEq_tQ.pNX1Se3pgyewMZ472Wx8syFFsxs" #トークン
 CHANNEL_ID = 793419776591396864 #チャンネルID
 
 GLOBAL_LIST = []
 GLOBAL_START_FLG = True
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print(client.users)
+    print('------')
+    
 
 @client.event
 async def on_message(message):
@@ -118,6 +119,8 @@ async def on_message(message):
                     # メッセージが送られてきたチャンネルへメッセージを送ります
                     await message.channel.send(m)
 
+
+
 # 60秒に一回ループ
 @tasks.loop(seconds=60)
 async def loop():
@@ -125,8 +128,8 @@ async def loop():
     now = datetime.now().strftime('%H:%M')
     # 現在の曜日
     week = datetime.now().strftime('%A')
-    if week == 'Saturday':
-        if now == '19:00':	
+    if week == 'Friday':
+        if now == '15:30':	
             global GLOBAL_START_FLG
             GLOBAL_START_FLG = False
             global GLOBAL_LIST
@@ -137,6 +140,5 @@ async def loop():
 
 #ループ処理実行
 loop.start()
-
-
-bot.run(token)
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
